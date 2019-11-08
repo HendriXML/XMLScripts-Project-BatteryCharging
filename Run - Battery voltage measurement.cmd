e3647355dfa95ce7fb770cc1bc3101f1e535f01a
HendriXML <40315442+HendriXML@users.noreply.github.com>
2019-11-08 20:56:47 +0100

    Initial scripts

---
+++ Run - Battery voltage measurement.cmd
@@ -0,0 +1,2 @@
+@echo off
+Executable\XMLScriptInterpreter.exe "Script.Project\Battery voltage measurement.xml" /ReportPath "C:\Reports\Battery voltage measurement"
---
+++ Run.cmd
@@ -0,0 +1,2 @@
+@echo off
+Executable\XMLScriptInterpreter.exe "Script.Project\NiMH Battery charging using a SPD3303X.xml" /ReportPath "C:\Reports\NiMH Battery charging"
