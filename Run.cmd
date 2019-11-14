d1a44154c6326a30e435cd6e05ce3131f67b2d42
HendriXML <40315442+HendriXML@users.noreply.github.com>
2019-11-14 22:30:58 +0100

    V5.2.26.2

--- Run.cmd
+++ Run.cmd
@@ -1,2 +1,2 @@
 @echo off
-Executable\XMLScriptInterpreter.exe "Script.Project\NiMH Battery charging using a SPD3303X.xml" /ReportPath "C:\Reports\NiMH Battery charging"
+Executable\XMLScriptInterpreter.exe "Script.Project\NiMH Battery charging using a Siglent power supply.xml" /ReportPath "C:\Reports\NiMH Battery charging"
